dcf0bccee8aee86d281bbdf6ace6518b3c4b0c99
Anmol Kumar <anmolkroe@gmail.com>
2026-07-11 23:48:08 +0530

    feat: add getMessages function for fetching chat messages from API

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,9 +1,17 @@
+import { MessagesSquare } from 'lucide-react'
 import React from 'react'
 
 function Nav() {
+  // const { conversations, selectedConversation } = useSelector(state => state.conversation);
+
   return (
-    <div>
-      
+    <div className="h-14 flex items-center gap-2.5 justify-between px-5 border-b border-white/[0.06] bg-[#0d0f14]">
+      <div>
+        <MessagesSquare/>
+      </div>
+      <div>
+        {/* {selectedConversation?.title || "New Chat"} */}
+      </div>
     </div>
   )
 }
