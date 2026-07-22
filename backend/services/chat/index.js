05c879b9cbe72719a8f21e331e9c3d04002155e8
Anmol Kumar <anmolkroe@gmail.com>
2026-07-22 14:50:27 +0530

    refactor: simplify chat service initialization and remove unused CORS configuration

--- backend/services/chat/index.js
+++ backend/services/chat/index.js
@@ -1,34 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
-import connectDb from "./config/db.js";
 import router from "./routes/chat.routes.js";
-;
+import connectDB from "./config/db.js";
+
 dotenv.config();
-
 const app = express();
-const PORT = process.env.PORT || 8000;
-
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
-
 app.use(express.json());
-
-
-
-
-app.get("/", (req, res) => {
-  res.send("Chat Service is running");
-});
+const port=process.env.PORT
 
 
 app.use("/",router)
 
-app.listen(PORT, () => {
-  console.log(`Chat Service is running on port ${PORT}`);
-  connectDb();
+
+app.listen(port, () => {
+    connectDB()
+  console.log(
+    `chat service running on ${port}`
+  );
 });
