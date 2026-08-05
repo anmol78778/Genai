dba03e134fec4e8eea0b2a71edc4069314ffd4d6
ak1947 <anmolkroe@gmail.com>
2026-08-05 14:39:39 +0530

    Update agent.route.js

--- backend/services/agent/routes/agent.route.js
+++ backend/services/agent/routes/agent.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { agent } from "../controllers/agent.controller.js";
-// import multer from "../config/multer.js";
+
 
 
 
@@ -9,7 +9,6 @@ express.Router();
 
 router.post(
  "/chat",
-//  multer.single("file"),
  agent
 );
 
